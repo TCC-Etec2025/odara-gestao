b0f3272c757e2727c54b53bb27fe381a1d813852
Nicole Bianca Cajueiro Eugênio <132144586+Nibianca@users.noreply.github.com>
2025-09-01 21:29:09 -0300

    Atualizando Nav sem algumas funcionalidades

--- src/components/Private/NavbarGestao.jsx
+++ src/components/Private/NavbarGestao.jsx
@@ -92,7 +92,7 @@ const NavbarGestao = ({ isCollapsed, setIsCollapsed, searchTerm, setSearchTerm }
         },
         {
           path: "./RegistroSaudeInicial",
-          label: "Registro de Saúde Inicial",
+          label: "Registro de Saúde Corporal",
           icon: (
             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
@@ -169,38 +169,6 @@ const NavbarGestao = ({ isCollapsed, setIsCollapsed, searchTerm, setSearchTerm }
           icon: (
             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>
-          )
-        },
-      ]
-    },
-    {
-      category: "Relações & Reuniões",
-      items: [
-        {
-          path: "./RegistroRelacoes",
-          label: "Registro de Relações",
-          icon: (
-            <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>
-          )
-        },
-        {
-          path: "./RegistroVideos",
-          label: "Registro de Vídeos dos Residentes",
-          icon: (
-            <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-            </svg>
-          )
-        },
-        {
-          path: "./PlataformaMeet",
-          label: "Reuniões",
-          icon: (
-            <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             </svg>
           )
         },
